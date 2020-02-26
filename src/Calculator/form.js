d914fc9dfd9497051800d1630c6e60659b11d20f
Sayan Sinha <sinhasayan888@gmail.com>
2020-02-27 01:14:19 +0530

    created functions for calculations

--- src/Calculator/form.js
+++ src/Calculator/form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Form = (calculate) =>{
+const Form = ({calculate, resultView}) =>{
     const [text1,setText1] = useState([]);
     const [text2,setText2] = useState([]);
 
@@ -11,20 +11,32 @@ const Form = (calculate) =>{
     const changeText2=(e)=>{
         setText2(e.target.value)  
     };
+
     return(
         <div>
             <form>
                 <input type = "text" value ={text1} placeholder="Operand 1" onChange={changeText1}/>
                 <input type = "text" value ={text2} placeholder="Operand 2" onChange={changeText2}/>
                 <br/>
+
                 <button onClick={()=>{
-                    calculate(text1, text2, 1)
+                    calculate(1, text1, text2);
                 }}>Add</button>
-                <button>Sub</button>
-                <button>Mul</button>
-                <button>Div</button>
+
+                <button onClick={()=>{
+                    calculate(2, text1, text2);
+                }}>Sub</button>
+
+                <button onClick={()=>{
+                    calculate(3, text1, text2);
+                }}>Mul</button>
+
+                <button onClick={()=>{
+                    calculate(4, text1, text2);
+                }}>Div</button>
+
                 <br/>
-                <input type="text" placeholder="Result" value={calculate.result}/>
+                <input type="text" placeholder="Result" value={resultView()} readOnly/>
             </form>
         </div>
     );
